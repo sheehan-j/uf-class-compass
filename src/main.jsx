441bcb79d78bd07a61d266cef9feff5823fe9c56
Jordan Sheehan <jordansheehan@ufl.edu>
2024-02-27 21:20:59 -0500

    Project structure setup

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./screens/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Router>
+			<Routes>
+				<Route path="/*" element={<Home />} />
+			</Routes>
+		</Router>
+	</React.StrictMode>
+);
