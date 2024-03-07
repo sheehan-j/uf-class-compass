8e775b80309f6ba51ffc96f65e9658f3f7360f6b
Jordan Sheehan <jordansheehan@ufl.edu>
2024-03-06 19:05:16 -0500

    Formatting changes

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Grid from "./Grid";
 import Colors from "../constants/Colors";
-import DaysBox from "./DaysBox"
-import {Days} from "../constants/Days"
-import {getPeriodTimes } from "../constants/BlockTimes";
+import DaysBox from "./DaysBox";
+import { Days } from "../constants/Days";
+import { getPeriodTimes } from "../constants/BlockTimes";
 
 const Schedule = ({ colCount, maxRowCount }) => {
 	const dummyClasses = [
@@ -16,19 +16,19 @@ const Schedule = ({ colCount, maxRowCount }) => {
 					col: Days.Monday,
 					row: 1, //TODO figure out if we want to zero index?
 					length: 1,
-					location: "FLG 0260"
+					location: "FLG 0260",
 				},
 				{
 					col: Days.Wednesday,
 					row: 3,
 					length: 1,
-					location: "FLG 0230"
+					location: "FLG 0230",
 				},
 				{
 					col: Days.Friday,
 					row: 3,
 					length: 1,
-					location: "FLG 0260"
+					location: "FLG 0260",
 				},
 			],
 		},
@@ -40,13 +40,13 @@ const Schedule = ({ colCount, maxRowCount }) => {
 					col: Days.Tuesday,
 					row: 4,
 					length: 2,
-					location: "CAR 0100"
+					location: "CAR 0100",
 				},
 				{
 					col: Days.Thursday,
 					row: 4,
 					length: 1,
-					location: "CAR 0100"
+					location: "CAR 0100",
 				},
 			],
 		},
@@ -58,35 +58,34 @@ const Schedule = ({ colCount, maxRowCount }) => {
 					col: Days.Monday,
 					row: 5,
 					length: 1,
-					location: "Online"
+					location: "Online",
 				},
 				{
 					col: Days.Wednesday,
 					row: 5,
 					length: 1,
-					location: "Online"
+					location: "Online",
 				},
 				{
 					col: Days.Friday,
 					row: 6,
 					length: 1,
-					location: "Online"
+					location: "Online",
 				},
 			],
-			
 		},
 	];
 
 	const [grid, setGrid] = useState([]);
 	const [classes] = useState(dummyClasses);
-	const [credits, setCredits] = useState(0)
-	const [newRowCount, setRowCount] = useState(maxRowCount)
+	const [credits, setCredits] = useState(0);
+	const [newRowCount, setRowCount] = useState(maxRowCount);
 
 	useEffect(() => {
 		const rows = [];
 		for (var i = 0; i < maxRowCount; i++) {
 			const row = [];
-			for (var j = 0; j < colCount; j++) row.push({ isClass: false, row: i+1 });
+			for (var j = 0; j < colCount; j++) row.push({ isClass: false, row: i + 1 });
 			rows.push(row);
 		}
 
@@ -96,7 +95,7 @@ const Schedule = ({ colCount, maxRowCount }) => {
 			classItem.meetings.forEach((meetingItem) => {
 				totalCredits += meetingItem.length;
 				rows[meetingItem.row][meetingItem.col] = {
-					row: meetingItem.row+1,
+					row: meetingItem.row + 1,
 					instructor: classItem.instructor,
 					isClass: true,
 					color: Colors.classColors[colorIndex],
@@ -107,7 +106,7 @@ const Schedule = ({ colCount, maxRowCount }) => {
 
 				for (var i = 1; i < meetingItem.length; i++) {
 					rows[meetingItem.row + i][meetingItem.col] = {
-						row: meetingItem.row+1,
+						row: meetingItem.row + 1,
 						isClass: true,
 						color: Colors.classColors[colorIndex],
 					};
@@ -116,14 +115,18 @@ const Schedule = ({ colCount, maxRowCount }) => {
 			colorIndex++;
 		});
 
-		while (rows.length > 1 && !rows[0].some(cell => cell.isClass) && !rows[1].some(cell => cell.isClass)) {
+		while (rows.length > 1 && !rows[0].some((cell) => cell.isClass) && !rows[1].some((cell) => cell.isClass)) {
 			rows.shift();
 		}
 
-		while (rows.length > 1 && !rows[rows.length-1].some(cell => cell.isClass) && !rows[rows.length-2].some(cell => cell.isClass)) {
+		while (
+			rows.length > 1 &&
+			!rows[rows.length - 1].some((cell) => cell.isClass) &&
+			!rows[rows.length - 2].some((cell) => cell.isClass)
+		) {
 			rows.pop();
 		}
-		
+
 		setRowCount(rows.length);
 		setGrid(rows);
 		setCredits(totalCredits);
@@ -131,40 +134,90 @@ const Schedule = ({ colCount, maxRowCount }) => {
 
 	return (
 		<>
-		<div style={{gap:"3%", display: "flex", flexDirection:"column", height: "100%", marginRight:"2%", width: "10%"}}>
-			<div className="font-medium" style={{ height:"5%", whiteSpace: "nowrap",  marginBottom: "5%", display: "flex", justifyContent: "center",alignItems: "center"}}>
-				CREDITS: {credits}
-			</div>
-			<div className="h-full" style={{display:"flex", flexDirection:"column"}}>
-				{grid.map((row) => (
-					<div className="relative" style={{display: 'flex', alignItems: "center",height: '100%' }}>
-						<div className="left-5 top-0 italic absolute" style={{ whiteSpace: "nowrap", fontSize: "0.9em", height:"100%"}}>
-							{getPeriodTimes(row[0].row).start}
+			<div
+				style={{
+					gap: "3%",
+					display: "flex",
+					flexDirection: "column",
+					height: "100%",
+					marginRight: "2%",
+					width: "10%",
+				}}
+			>
+				<div
+					className="font-medium"
+					style={{
+						height: "5%",
+						whiteSpace: "nowrap",
+						marginBottom: "5%",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				>
+					CREDITS: {credits}
+				</div>
+				<div className="h-full" style={{ display: "flex", flexDirection: "column" }}>
+					{grid.map((row) => (
+						<div className="relative" style={{ display: "flex", alignItems: "center", height: "100%" }}>
+							<div
+								className="left-5 top-0 italic absolute"
+								style={{ whiteSpace: "nowrap", fontSize: "0.9em", height: "100%" }}
+							>
+								{getPeriodTimes(row[0].row).start}
+							</div>
+							<div
+								style={{
+									flex: "1",
+									display: "flex",
+									justifyContent: "right",
+									alignItems: "center",
+									height: "100%",
+								}}
+							>
+								{row[0].row}
+							</div>
 						</div>
-						<div style={{flex: "1", display: 'flex', justifyContent: 'right', alignItems: 'center', height: '100%'}}>
-							{row[0].row}
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-
-		<div style={{ height: "100%", display: "flex", flexDirection: "column", width: "100%"}}>
-			<div className="grid w-full relative" style={{ marginBottom: "3%", height: "5%", gridTemplateColumns: `repeat(${colCount}, minmax(0, 1fr))`, gridTemplateRows: `repeat(${1}, minmax(0, 1fr))`}}>
-				<DaysBox/>
+					))}
+				</div>
 			</div>
 
-			<div className="w-full h-full border border-slate-300 relative">
-				{/* 
+			<div style={{ height: "100%", display: "flex", flexDirection: "column", width: "100%" }}>
+				<div
+					className="grid w-full relative"
+					style={{
+						marginBottom: "3%",
+						height: "5%",
+						gridTemplateColumns: `repeat(${colCount}, minmax(0, 1fr))`,
+						gridTemplateRows: `repeat(${1}, minmax(0, 1fr))`,
+					}}
+				>
+					<DaysBox />
+				</div>
+
+				<div className="w-full h-full border border-slate-300 relative">
+					{/* 
 				There are TWO grids, one is a skeleton that creates the grid lines,
 				the other is the grid actually containing the classes and their colored
 				rectangles. This is done to achieve the visual effect of the grid lines
 				behind the transparent class boxes.
 				*/}
-				<Grid key={"skeletonGrid"} grid={grid} colCount={colCount} rowCount={newRowCount} isSkeleton={false} />
-				<Grid key={"coloredGrid"} grid={grid} colCount={colCount} rowCount={newRowCount} isSkeleton={true} />
+					<Grid
+						key={"skeletonGrid"}
+						grid={grid}
+						colCount={colCount}
+						rowCount={newRowCount}
+						isSkeleton={false}
+					/>
+					<Grid
+						key={"coloredGrid"}
+						grid={grid}
+						colCount={colCount}
+						rowCount={newRowCount}
+						isSkeleton={true}
+					/>
+				</div>
 			</div>
-		</div>
 		</>
 	);
 };
