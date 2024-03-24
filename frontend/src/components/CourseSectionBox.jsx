184cba9bc67eebf707fd12f9f3d49f3feaa32fca
Jordan Sheehan <jordansheehan@ufl.edu>
2024-03-24 16:17:27 -0400

    Use padding to separate course section boxes instead of margin

--- frontend/src/components/CourseSectionBox.jsx
+++ frontend/src/components/CourseSectionBox.jsx
@@ -13,22 +13,29 @@ const CourseSectionBox = ({
 	};
 
 	return (
+		// Use a wrapper around the main element with padding so there is a visual gap between the boxes, but there is no
+		// actual gap in between each box. This avoids a flicker effect when you move your mouse down each of the course section
+		// boxes because of the small gap in between each box where you wouldn't be hovering over anything if margin was used
 		<div
-			className={`relative border border-gray-300 py-2 px-3 mb-1 flex items-center justify-between cursor-pointer bg-white`}
+			className="pb-1"
 			onMouseEnter={() => handleHoverClassStart(classItem)}
 			onMouseLeave={() => handleHoverClassEnd(classItem)}
 		>
-			<p className="text-sm">{`Class #${classItem.number}`}</p>
-			<button style={{ width: "1.1rem" }} onClick={addClass}>
-				{activeSchedule?.classes?.some(
-					(activeScheduleClass) =>
-						activeScheduleClass._id == classItem._id && activeScheduleClass?.muteInActiveCourses != true
-				) ? (
-					<img src="/check.svg" />
-				) : (
-					<img src="/add.svg" />
-				)}
-			</button>
+			<div
+				className={`relative border border-gray-300 py-2 px-3 flex items-center justify-between cursor-pointer bg-white`}
+			>
+				<p className="text-sm">{`Class #${classItem.number}`}</p>
+				<button style={{ width: "1.1rem" }} onClick={addClass}>
+					{activeSchedule?.classes?.some(
+						(activeScheduleClass) =>
+							activeScheduleClass._id == classItem._id && activeScheduleClass?.muteInActiveCourses != true
+					) ? (
+						<img src="/check.svg" />
+					) : (
+						<img src="/add.svg" />
+					)}
+				</button>
+			</div>
 		</div>
 	);
 };
