551cfdc11aca15cbd952e2d5b46e51018628ff3e
Jordan Sheehan <jordansheehan@ufl.edu>
2024-03-27 14:29:54 -0400

    Add comment to API config

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -6,4 +6,5 @@ const dev = {
 	API_BASE_URL: "http://localhost:6205/api",
 };
 
+// No need to add NODE_ENV to .env for this, it is auto-configured with Vite
 export const config = process.env.NODE_ENV === "development" ? dev : prod;
