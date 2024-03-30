9c1eeebc15315060d6e74efa4ea84642cd814408
esteban <estebancalvo0808@gmail.com>
2024-03-30 02:00:21 -0400

    started frontend for course search

--- frontend/src/constants/StyleColors.js
+++ frontend/src/constants/StyleColors.js
@@ -1,7 +1,8 @@
 const StyleColors = {
     blue: "rgb(40, 87, 151)",
     orange: "rgb(235, 120, 23)",
-    gray: "rgb(235, 235, 235)"
+    gray: "rgb(235, 235, 235)",
+    beige: "rgba(254, 210, 126, 0.3)",
 }
 
 export default StyleColors;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Home from "./screens/Home";
 import SchedulePage from "./screens/SchedulePage";
 import UserPage from "./screens/UserPage";
+import CourseSearch from "./screens/CourseSearch";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 				<Route path="/*" element={<Home />} />
 				<Route path="/SchedulePage" element={<SchedulePage />} />
 				<Route path="/UserPage" element={<UserPage />} />
+				<Route path="/CourseSearch" element={<CourseSearch />} />
 			</Routes>
 		</Router>
 	</React.StrictMode>
