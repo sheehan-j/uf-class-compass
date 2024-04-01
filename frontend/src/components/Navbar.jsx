c3b01babbde125a4b0499308152678eb498ed978
esteban <estebancalvo0808@gmail.com>
2024-04-01 16:14:11 -0400

    fixed navbar styling to only use tailwind

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,45 +1,13 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {useState } from "react";
 import { Link } from "react-router-dom";
 import StyleColors from "../constants/StyleColors";
 
 const Navbar = () => {
-  const buttonsRef = useRef([]);
-
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const [isMobileScreen, setIsMobileScreen] = useState(window.matchMedia("(max-width: 640px)").matches)
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobileScreen(window.matchMedia("(max-width: 640px)").matches);
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen((prev) => !prev);
   };
 
-
-  useEffect(() => {
-      if (isMobileScreen) {
-          return;
-      }
-      console.log("here");
-        if(buttonsRef.current && buttonsRef.current.length > 0){
-          const maxWidth = Math.max(
-            ...buttonsRef.current.map((button) => button.offsetWidth)
-          );
-
-          buttonsRef.current.forEach((button) => {
-            button.style.width = `${maxWidth}px`;
-          });
-      }
-  }, [buttonsRef, isMobileScreen]);
 
   return (
     <header className="w-full text-white" style={{ backgroundColor: StyleColors.blue }}>
@@ -48,20 +16,20 @@ const Navbar = () => {
         <div className="block w-full h-10 sm:hidden">
           <img className="float-left h-full" src="/mobileMenu.svg" onClick={toggleMobileMenu}/>
         </div>
-        <div className={`${!isMobileMenuOpen ? "hidden" : "flex"} items-center w-full sm:w-auto sm:flex flex-col sm:flex-row sm:gap-5`}>
-          <Link className={`link-item  mb-2 sm:mb-0 ${isMobileScreen ? "w-3/5" : "" }`} to="/">
-            <button className="py-2.5 px-5 rounded-lg w-full" style={{ backgroundColor: StyleColors.orange }} ref={(el) => (buttonsRef.current[0] = el)}>Home</button>
+        <div className={`${!isMobileMenuOpen ? "hidden" : "flex"} items-center w-full sm:w-auto sm:flex flex-col sm:flex-row sm:grid sm:grid-cols-3 sm:gap-5`}>
+          <Link className={`link-item  mb-2 sm:mb-0 w-3/5 sm:w-full`} to="/">
+            <button className="py-2.5 px-5 rounded-lg w-full" style={{ backgroundColor: StyleColors.orange }}>Home</button>
           </Link>
-          <Link className={`link-item  mb-2 sm:mb-0 ${isMobileScreen ? "w-3/5" : "" }`} to="/SchedulePage">
-            <button className="py-2.5 px-5 rounded-lg w-full" style={{ backgroundColor: StyleColors.orange }} ref={(el) => (buttonsRef.current[1] = el)}>Schedule</button>
+          <Link className={`link-item  mb-2 sm:mb-0 w-3/5 sm:w-full`} to="/SchedulePage">
+            <button className="py-2.5 px-5 rounded-lg w-full" style={{ backgroundColor: StyleColors.orange }}>Schedule</button>
           </Link>
-          <Link className={`link-item  mb-2 sm:mb-0 ${isMobileScreen ? "w-3/5" : "" }`} to="/CourseSearch">
-            <button className="py-2.5 px-5 rounded-lg w-full" style={{ backgroundColor: StyleColors.orange }} ref={(el) => (buttonsRef.current[2] = el)}>Course Search</button>
+          <Link className={`link-item  mb-2 sm:mb-0 w-3/5 sm:w-full`} to="/CourseSearch">
+            <button className="py-2.5 px-5 rounded-lg w-full" style={{ backgroundColor: StyleColors.orange }}>Course Search</button>
           </Link>
         </div>
         <div className={`sm:w-auto ${!isMobileMenuOpen ? "hidden" : "flex"} justify-center w-full sm:flex sm:w-auto`}>
-          <Link className={`link-item ${isMobileScreen ? "w-3/5" : "" }`} to="/UserPage">
-            <button className="py-2.5 px-5 rounded-lg w-full" style={{ backgroundColor: StyleColors.orange }} ref={(el) => (buttonsRef.current[3] = el)}>Login</button>
+          <Link className={`link-item w-3/5 sm:w-full`} to="/UserPage">
+            <button className="py-2.5 px-5 rounded-lg w-full" style={{ backgroundColor: StyleColors.orange }}>Login</button>
           </Link>
         </div>
       </div>
--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -5,25 +5,25 @@ const Home = () => {
     return(
         <div>
             <Navbar />
-            <div className="border-b flex justify-center items-center p-10">
-                <div className="p-5" style={{width: "60%"}}>
+            <div className="border-b flex flex-col sm:flex-row justify-center items-center p-10">
+                <div className="p-5 w-full sm:w-7/12 text-center sm:text-left">
                     <h1 className="text-5xl font-bold pb-10">UF Class Compass</h1>
                     <h3 className="text-xl pb-10">Plan your schedule with ease</h3>
-                    <Link className="link-item" to="/">
+                    <Link className="link-item" to="/SchedulePage">
                         <button className="py-2.5 px-5 rounded-lg text-white text-xl" style={{backgroundColor: StyleColors.orange}}>Start Building Your Schedule Now</button>
                     </Link>
                 </div>
-                <div style={{width: "25%", height: "auto"}}>
+                <div className="hidden sm:block sm:w-4/12 h-auto">
                     <img src="/working.png" className="w-full h-auto" alt="Logo"/>
                 </div>
             </div>
             <div className="p-10" style={{backgroundColor: StyleColors.gray}}>
                 <h1 className="text-center w-full text-3xl font-bold pb-5" style={{color: StyleColors.orange}}>What is UF Class Compass?</h1>
-                <div className="flex justify-center gap-10">
-                    <div className="w-2/5">
-                        <img src="/homeExample.png"/>
+                <div className="flex-row md:flex md:justify-center md:gap-10">
+                    <div className="w-full md:w-2/5 md:item-center">
+                        <img className='h-full object-scale-down' src="/homeExample.png"/>
                     </div>
-                    <div className="w-2/5 space-y-5">
+                    <div className="w-full pt-4 md:pt-0 md:w-2/5 space-y-5">
                         <div>
                             <h1 style={{color: StyleColors.orange}}>Your One-Stop Hub For All Things Scheduling</h1>
                             <p>Class Compass serves as a centralized platform for all tools needed to streamline the scheduling process every semester.</p>
