b503bea16a4b6bb1a200d8ffea8cb2a74ee28182
Jordan Sheehan <jordansheehan@ufl.edu>
2024-04-03 21:57:13 -0400

    Connect login/signup to backend

--- frontend/src/screens/UserPage.jsx
+++ frontend/src/screens/UserPage.jsx
@@ -1,100 +1,221 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import StyleColors from "../constants/StyleColors";
+import { useAuth } from "../hooks/AuthProvider";
+import { UserApi } from "../api/UserApi";
 
 const UserPage = () => {
-    const [isSignIn, setSignIn] = useState(true);
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+	const [isSignIn, setSignIn] = useState(true);
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [error, setError] = useState("");
+	const auth = useAuth();
 
+	const handleSignUp = async (event) => {
+		event.preventDefault();
+		setError("");
 
-    const handleSignUp = async (event) => {
-        event.preventDefault();
-    }
+		if (password !== confirmPassword) {
+			setError("Passwords must match.");
+			return;
+		}
 
-    const handleSignIn = async (event) => {
-        event.preventDefault();
-        // try {
-        //   const response = await fetch('/api/login', {
-        //     method: 'POST',
-        //     headers: {
-        //       'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify({ username, password }),
-        //   });
-    
-        //   if (response.ok) {
-        //     console.log('Login successful');
-        //   } else {
-        //     const errorData = await response.json();
-        //     console.error('Login failed:', errorData.message);
-        //   }
-        // } catch (error) {
-        //   console.error('Login error:', error);
-        // }
-      };
+		const signUpResponse = await UserApi.register({
+			firstName: firstName,
+			lastName: lastName,
+			email: email,
+			password: password,
+		});
 
-      const handleSubmit = isSignIn ? handleSignIn : handleSignUp;
+		if (signUpResponse.status === 201) {
+			handleSignIn(null);
+		} else {
+			setError(signUpResponse.error);
+		}
+	};
 
+	const handleSignIn = async (event) => {
+		event?.preventDefault();
+		setError("");
 
+		try {
+			await auth.login({ email: email, password: password });
+		} catch (err) {
+			setError(err.message);
+		}
+	};
+
+	const switchForms = (changeToSignIn) => {
+		setSignIn(changeToSignIn);
+		setError("");
+		setFirstName("");
+		setLastName("");
+		setEmail("");
+		setPassword("");
+		setConfirmPassword("");
+		setError("");
+	};
+
+	const handleSubmit = isSignIn ? handleSignIn : handleSignUp;
 
 	return (
-        <>
-        <Navbar />
-		<div className="w-full h-full flex pt-10">
-			<div className="w-full flex justify-center">
-                <div className="w-full sm:w-3/4 lg:w-4/6 h-fit pb-10 rounded-lg shadow dark:border text-white" style={{backgroundColor : StyleColors.blue}}>
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8 text-white">
-                        <div className="signInButtons flex justify-center">
-                            <button style={{ backgroundColor: isSignIn ? StyleColors.orange : "white", color: isSignIn ? "white" : "black" }} className="rounded-l-lg p-3 text-xl font-bold leading-tight tracking-tight md:text-2xl dark:text-black" onClick={() => setSignIn(true)} id={isSignIn ? "active" : ""}>
-                                    Sign in
-                            </button>
-                            <button style={{ backgroundColor: !isSignIn ? StyleColors.orange : "white", color: !isSignIn ? "white" : "black" }} className="rounded-r-lg p-3 text-xl font-bold leading-tight tracking-tight md:text-2xl dark:text-black" onClick={() => setSignIn(false)} id={!isSignIn ? "active" : ""}>
-                                    Sign Up
-                            </button>
-                        </div>
-                        <div className="w-full flex justify-center">
-                            {isSignIn ? (
-                                <div className="p-3 text-xl font-bold leading-tight tracking-tight  md:text-l flex">
-                                    Welcome Back!
-                                </div>
-                            ) : (
-                                <div className="p-3 text-xl font-bold leading-tight tracking-tight md:text-l text-white">
-                                    Create an Account
-                                </div>
-                            )}
-                        </div>
-                        <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
-                            <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium ">Username</label>
-                                <input type="username" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} className="bg-white border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-black" placeholder="" required=""/>
-                            </div>
-                            <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium">Password</label>
-                                <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="bg-white border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-black" required=""/>
-                            </div>
-                            {!isSignIn && (
-                            <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium">Confirm Password</label>
-                                <input type="password" name="password" id="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className="bg-white border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-black" required=""/>
-                            </div>
-                            )}
-                            <div className="flex justify-center submitButtons">
-                                {isSignIn ? (
-                                    <button type="submit" style={{backgroundColor: StyleColors.orange}} className="bg-teal-500 rounded-lg  w-1/2 p-3 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                        Sign in
-                                </button>) : (
-                                    <button type="submit" style={{backgroundColor: StyleColors.orange}} className="bg-teal-500 w-1/2 rounded-lg p-3 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                        Sign up
-                                </button>)}
-                            </div>
-                        </form>
-                    </div>
-                </div>
+		<>
+			<Navbar />
+			<div className="w-full h-full flex pt-10">
+				<div className="w-full flex justify-center">
+					<div
+						className="w-full sm:w-3/4 lg:w-4/6 h-fit pb-10 rounded-lg shadow dark:border text-white"
+						style={{ backgroundColor: StyleColors.blue }}
+					>
+						<div className="p-6 space-y-4 md:space-y-6 sm:p-8 text-white">
+							<div className="signInButtons flex justify-center">
+								<button
+									style={{
+										backgroundColor: isSignIn ? StyleColors.orange : "white",
+										color: isSignIn ? "white" : "black",
+									}}
+									className="rounded-l-lg p-3 text-xl font-bold leading-tight tracking-tight md:text-2xl dark:text-black"
+									onClick={() => {
+										switchForms(true);
+									}}
+									id={isSignIn ? "active" : ""}
+								>
+									Sign in
+								</button>
+								<button
+									style={{
+										backgroundColor: !isSignIn ? StyleColors.orange : "white",
+										color: !isSignIn ? "white" : "black",
+									}}
+									className="rounded-r-lg p-3 text-xl font-bold leading-tight tracking-tight md:text-2xl dark:text-black"
+									onClick={() => {
+										switchForms(false);
+									}}
+									id={!isSignIn ? "active" : ""}
+								>
+									Sign Up
+								</button>
+							</div>
+							<div className="w-full flex justify-center">
+								{isSignIn ? (
+									<div className="p-3 text-xl font-bold leading-tight tracking-tight  md:text-l flex">
+										Welcome Back!
+									</div>
+								) : (
+									<div className="p-3 text-xl font-bold leading-tight tracking-tight md:text-l text-white">
+										Create an Account
+									</div>
+								)}
+							</div>
+							<form onSubmit={handleSubmit} className="space-y-4 md:space-y-6">
+								{!isSignIn && (
+									<div className="flex gap-4">
+										<div className="grow">
+											<label htmlFor="firstName" className="block mb-2 text-sm font-medium ">
+												Firstname
+											</label>
+											<input
+												type="text"
+												name="firstName"
+												id="firstName"
+												value={firstName}
+												onChange={(e) => setFirstName(e.target.value)}
+												className="bg-white border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-black"
+												placeholder=""
+												required
+											/>
+										</div>
+										<div className="grow">
+											<label htmlFor="lastName" className="block mb-2 text-sm font-medium ">
+												Lastname
+											</label>
+											<input
+												type="text"
+												name="lastName"
+												id="lastName"
+												value={lastName}
+												onChange={(e) => setLastName(e.target.value)}
+												className="bg-white border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-black"
+												placeholder=""
+												required
+											/>
+										</div>
+									</div>
+								)}
+								<div>
+									<label htmlFor="email" className="block mb-2 text-sm font-medium ">
+										Email
+									</label>
+									<input
+										type="email"
+										name="email"
+										id="email"
+										value={email}
+										onChange={(e) => setEmail(e.target.value)}
+										className="bg-white border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-black"
+										placeholder=""
+										required
+									/>
+								</div>
+								<div>
+									<label htmlFor="password" className="block mb-2 text-sm font-medium">
+										Password
+									</label>
+									<input
+										type="password"
+										name="password"
+										id="password"
+										value={password}
+										onChange={(e) => setPassword(e.target.value)}
+										className="bg-white border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-black"
+										required
+									/>
+								</div>
+								{!isSignIn && (
+									<div>
+										<label htmlFor="password" className="block mb-2 text-sm font-medium">
+											Confirm Password
+										</label>
+										<input
+											type="password"
+											name="password"
+											id="confirmPassword"
+											value={confirmPassword}
+											onChange={(e) => setConfirmPassword(e.target.value)}
+											className="bg-white border border-gray-300 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 text-black"
+											required
+										/>
+									</div>
+								)}
+								{error && <div>Error: {error}</div>}
+								<div className="flex justify-center submitButtons">
+									{isSignIn ? (
+										<button
+											type="submit"
+											style={{ backgroundColor: StyleColors.orange }}
+											className="bg-teal-500 rounded-lg  w-1/2 p-3 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+										>
+											Sign in
+										</button>
+									) : (
+										<button
+											type="submit"
+											style={{ backgroundColor: StyleColors.orange }}
+											className="bg-teal-500 w-1/2 rounded-lg p-3 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+										>
+											Sign up
+										</button>
+									)}
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
-        </>
+		</>
 	);
 };
 
