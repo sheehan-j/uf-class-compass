ec1b0d7b8eb3584d2b4aa47b6b3770b5428e600e
VBoudreau55 <vancebou55@gmail.com>
2024-04-05 16:46:54 -0400

    finished data pull without api calls

--- backend/scripts/pullData.js
+++ backend/scripts/pullData.js
@@ -1,9 +1,70 @@
-//Grab all uf courses
-//place into objects with proper formatting
-//grab all instructors in seperate data structure
-//run add instructor api until all instructors
-//store all the buildings codes into a data structure
-const API_KEY = require('./key.js');
+const API_KEY = require("./key.js");
+const fs = require("fs");
+
+const buildingMap = createMapFromJSON("buildings.json");
+function interpretday(day) {
+  switch (day) {
+    case "M":
+      return 1;
+    case "T":
+      return 2;
+    case "W":
+      return 3;
+    case "R":
+      return 4;
+    case "F":
+      return 5;
+    case "S":
+      return 6;
+
+    default:
+      return 7;
+  }
+}
+async function readFile() {
+  // Function to read the file and call async function for each line
+
+  const courseCodes = [];
+  try {
+    const data = fs.readFileSync(filename, "utf8");
+    const lines = data.split("\n");
+
+    for (let line of lines) {
+      // Trim the line to remove any leading/trailing whitespace
+      line = line.trim();
+
+      // Check if the line is not empty
+      if (line) {
+        // Call your async function for each line
+        courseCodes.push(line);
+      }
+    }
+    console.log("Processing complete.");
+  } catch (err) {
+    console.error("Error reading the file:", err);
+  }
+}
+
+function createMapFromJSON(filePath) {
+  // Read the JSON file synchronously
+  const jsonData = fs.readFileSync(filePath, "utf8");
+
+  // Parse the JSON data into an array of objects
+  const jsonArray = JSON.parse(jsonData);
+
+  // Create a new Map
+  const map = new Map();
+
+  // Iterate over the array of objects and populate the Map
+  jsonArray.forEach((obj) => {
+    // Assuming each object has an "ID" attribute
+    const id = obj.ID;
+    map.set(id, obj);
+  });
+
+  // Return the reference to the Map
+  return map;
+}
 async function searchPlaces(query) {
   // Constructing the API URL with the provided query and API key
   const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&key=${API_KEY}`;
@@ -18,27 +79,32 @@ async function searchPlaces(query) {
     })
     .then((data) => {
       // Handling the response data
-      var firstResult = data.results[0];
-      // Accessing the formatted address
-      var formattedAddress_ = firstResult.formatted_address;
-      // Accessing the location (latitude and longitude)
-      var location = firstResult.geometry.location;
-      var latitude_ = location.lat;
-      var longitude_ = location.lng;
+      data.results.forEach((result) => {
+        // Accessing the formatted address
+        var location = result.geometry.location;
+        var latitude_ = location.lat;
+        var longitude_ = location.lng;
+        if (
+          latitude_ >= 29 && //make sure it's in gaines
+          latitude_ <= 30 &&
+          longitude_ >= -83 &&
+          longitude_ <= -82
+        ) {
+          var formattedAddress_ = result.formatted_address;
+          var name_ = result.name;
+          var placeId_ = result.place_id;
 
-      // Accessing the name
-      var name_ = firstResult.name;
-      // Accessing the place ID
-      var placeId_ = firstResult.place_id;
+          var place = {
+            name: name_,
+            placeId: placeId_,
+            formattedAddress: formattedAddress_,
+            latitude: latitude_,
+            longitude: longitude_,
+          };
+          return place;
+        }
+      });
 
-      var place = {
-        name: name_,
-        placeId: placeId_,
-        formattedAddress: formattedAddress_,
-        latitude: latitude_,
-        longitude: longitude_,
-      }
-    return place;
       // You can further process the data here
     })
     .catch((error) => {
@@ -46,73 +112,131 @@ async function searchPlaces(query) {
     });
 }
 
-async function searchBuildings() {
-    // Constructing the API URL with the provided query and API key
-    const apiUrl = `https://campusmap.ufl.edu/library/cmapjson/search.json`;
-    // Making the API request
-    return await fetch(apiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        // Handling the response data
-        
-      return data;
-        // You can further process the data here
-      })
-      .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
-      });
+async function searchClass(courseCode) {
+  // Constructing the API URL with the provided query and API key
+  const apiUrl = `https://one.ufl.edu/apix/soc/schedule/?category=RES&term=2248&course-code=${courseCode}`;
+  // Making the API request
+  return await fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (
+        data &&
+        Array.isArray(data) &&
+        data.length > 0 &&
+        data[0].COURSES &&
+        Array.isArray(data[0].COURSES) &&
+        data[0].COURSES.length > 0
+      ) {
+        const course = data[0].COURSES[0];
+        return course;
+      } else {
+        console.log(
+          `COURSES array is empty or not found for class ${courseCode}. Skipping...`
+        );
+      }
+      // You can further process the data here
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
+
+async function getInstructors(instructors) {
+  //get list of instructors in param
+  //for each one attempt to add an instructor to the DB or link to an existing instructor in the DB
+  //return that list?
+
+  instructors.forEach(instructor =>{
+    const instructorUpload = {
+      name: instructor,
+    };
+
+    //TODO add in call to create instructor API
+  });
+}
+
+async function getBuilding(buildingCode, buildingCodeLetters) {
+  //get building code from map
+  //search building up with google query searchPlace() func
+  //return the building ?
+  const buildingObj = buildingMap.get(buildingCode);
+  const placeObj = searchPlaces(buildingObj.NAME);
+
+  const uploadedBuilding ={ //TODO change this to match the DB schema
+    code: buildingCodeLetters,
+    pid: placeObj.placeId,
+    name: buildingObj.NAME,
+    //long: buildingObj.LON,
+    //lat: buildingObj.LAT,
+    //bid: buildingObj.ID
   }
 
-  async function searchClass(courseCode) {
-    // Constructing the API URL with the provided query and API key
-    const apiUrl = `https://one.ufl.edu/apix/soc/schedule/?category=RES&term=2238&course-code=${courseCode}`;
-    // Making the API request
-    return await fetch(apiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        if (
-            data &&
-            Array.isArray(data) &&
-            data.length > 0 &&
-            data[0].COURSES &&
-            Array.isArray(data[0].COURSES) &&
-            data[0].COURSES.length > 0
-          ) {
-            const course = data[0].COURSES[0];
-            return course;
-          } else {
-            console.log(
-              `COURSES array is empty or not found for class ${classId}. Skipping...`
-            );
-          }
-        // You can further process the data here
-      })
-      .catch((error) => {
-        console.error("There was a problem with the fetch operation:", error);
+
+  //TODO add in call to create building API
+}
+
+async function collectData() {
+  //Grab all uf courses
+  //place into objects with proper formatting
+  //grab all instructors in seperate data structure
+  //run add instructor api until all instructors
+  //store all the buildings codes into a data structure
+
+  const courseCodes = await readFile();
+
+  courseCodes.forEach(async (courseCode) => {
+    const course = await searchClass(courseCode);
+
+    const instructors = [];
+    const courseUpload = {};
+
+    courseUpload.code = course.code;
+    courseUpload.title = course.name;
+    courseUpload.description = course.description;
+    courseUpload.prereq = course.prerequisites;
+
+    course.sections.forEach(async (section) => {
+      var classSectionUpload = {};
+      section.instructors.forEach((instructor) => {
+        instructors.push(instructor);
       });
-  }
-// Example usage
-const query = "Florida Gymnasium"; // Replace with your query
 
+      classSectionUpload.number = section.classNumber;
+      classSectionUpload.title = course.name;
+      //add instructor(s)
+      classSectionUpload.instructors = await getInstructors(instructors);
 
+      section.meetings.forEach(async (meeting) => {
+        var meetDay;
+        section.meetDays.forEach(async (day) => {
+          meetDay = interpretday(day);
+          classSectionUpload.meetings.push({
+            day: meetDay,
+            period: meeting.meetPeriodBegin,
+            length: meeting.meetPeriodEnd - meeting.meetPeriodBegin + 1,
+            room: meeting.meetRoom,
+            building: await getBuilding(meeting.meetBldgCode, meeting.meetBuilding),
+          });
+        });
+      });
+      
+      //TODO call create class API statement for classSectionUpload
+    });
 
+    //TODO call create course API call for courseUpload
+  });
+}
 
+//Function Caller
 (async () => {
-    try {
-        const place = await searchPlaces(query);
-        //const buildings = await searchBuildings();
-        console.log(place);
-    } catch (error) {
-        console.error("Error occurred:", error);
-    }
+  try {
+    //collectData();
+  } catch (error) {
+    console.error("Error occurred:", error);
+  }
 })();
