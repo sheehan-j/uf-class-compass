242e6a63222c724d66d680ea3b8fe8f386dc7096
Jordan Sheehan <jordansheehan@ufl.edu>
2024-04-11 19:15:23 -0400

    Small navbar styling changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,39 +10,76 @@ const Navbar = () => {
 	};
 	const auth = useAuth();
 
-  return (
-    <header className="w-full text-white relative" style={{ backgroundColor: StyleColors.blue }}>
-      <nav className="w-full relative" style={{ borderBottom: "1px solid rgba(235,235,235, 0.5)" }}>
-        <div className="flex flex-col sm:flex-row justify-between items-center px-10 py-5">
-          <div className="block w-full h-10 sm:hidden">
-            <img className="float-left h-full" src="/mobileMenu.svg" onClick={toggleMobileMenu} />
-          </div>
-          <div className={`${!isMobileMenuOpen ? "hidden" : "flex"} items-center w-auto sm:w-auto sm:flex flex-col sm:flex-row sm:grid sm:grid-cols-4 sm:gap-5`}>
-            <Link className={`link-item mb-2 sm:mb-0 w-4/5 md:w-2/5 max-sm:hidden flex items-center `} to="/">
-              <img className="md:w-auto px-5 object-scale-down" id="logo" src="/CourseCompassLogo.png" alt="Logo" />
-              <span className="ml-2 max-md:hidden font-bold text-start md:text-l lg:text-2xl">UF Class Compass</span>
-            </Link>
-            <Link className={`link-item  mb-2 sm:mb-0 w-3/5 sm:w-full sm:hidden`} to="/">
-              <button className="py-2.5 px-5 rounded-lg w-full" style={{ backgroundColor: StyleColors.orange }}>Home</button>
-            </Link>
-            <Link className={`link-item  mb-2 sm:mb-0 w-3/5 sm:w-full`} to="/SchedulePage">
-              <button className="py-2.5 px-5 rounded-lg w-full" style={{ backgroundColor: StyleColors.orange }}>Schedule</button>
-            </Link>
-            <Link className={`link-item  mb-2 sm:mb-0 w-3/5 sm:w-full`} to="/CourseSearch">
-              <button className="py-2.5 px-5 rounded-lg w-full" style={{ backgroundColor: StyleColors.orange }}>Course Search</button>
-            </Link>
-          </div>
-          <div className={`sm:w-auto ${!isMobileMenuOpen ? "hidden" : "flex"} justify-center w-full sm:flex sm:w-auto`}>
-            <Link className={`link-item w-3/5 sm:w-full`} to="/UserPage">
-              <button className="py-2.5 px-5 rounded-lg w-full" style={{ backgroundColor: StyleColors.orange }}>Login</button>
-            </Link>
-          </div>
-        </div>
-      </nav>
-    </header>
+	return (
+		<header className="w-full text-white relative" style={{ backgroundColor: StyleColors.blue }}>
+			<nav className="w-full relative" style={{ borderBottom: "1px solid rgba(235,235,235, 0.5)" }}>
+				<div className="flex flex-col sm:flex-row justify-between items-center py-3 px-5">
+					<div className="block w-full h-10 sm:hidden">
+						<img className="float-left h-full" src="/mobileMenu.svg" onClick={toggleMobileMenu} />
+					</div>
+					<div
+						className={`${
+							!isMobileMenuOpen ? "hidden" : "flex"
+						} items-center w-full sm:flex flex-col sm:flex-row sm:justify-between sm:gap-5`}
+					>
+						<Link className={`link-item mb-2 sm:mb-0 max-sm:hidden flex items-center`} to="/">
+							<img
+								className="w-16 sm:w-28 px-5 object-scale-down"
+								id="logo"
+								src="/CourseCompassLogo.png"
+								alt="Logo"
+							/>
+							<span className="max-md:hidden font-bold text-start md:text-l lg:text-2xl">
+								UF Class Compass
+							</span>
+						</Link>
 
-
-  );
+						<div className="flex flex-col w-full mt-2 sm:mt-0 sm:w-auto sm:flex-row gap-3">
+							<Link className={`link-item w-full sm:w-auto sm:hidden`} to="/">
+								<button
+									className="py-2.5 px-5 rounded-lg w-full"
+									style={{ backgroundColor: StyleColors.orange }}
+								>
+									Home
+								</button>
+							</Link>
+							<Link className={`link-item`} to="/schedule">
+								<button
+									className="py-2.5 px-10 rounded-lg w-full"
+									style={{ backgroundColor: StyleColors.orange }}
+								>
+									Schedule
+								</button>
+							</Link>
+							<Link className={`link-item`} to="/search">
+								<button
+									className="py-2.5 px-10 rounded-lg w-full"
+									style={{ backgroundColor: StyleColors.orange }}
+								>
+									Course Search
+								</button>
+							</Link>
+							<button
+								className="py-2.5 px-5 rounded-lg w-full sm:w-auto"
+								style={{ backgroundColor: StyleColors.orange }}
+								onClick={
+									auth?.user
+										? () => {
+												auth.logout();
+										  }
+										: () => {
+												auth.login();
+										  }
+								}
+							>
+								{auth?.user ? "Logout" : "Login"}
+							</button>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</header>
+	);
 };
 
 export default Navbar;
