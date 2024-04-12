57fc85a76ef6fc4caa2ce27e0f649bc764cceab9
Jordan Sheehan <jordansheehan@ufl.edu>
2024-04-11 21:15:57 -0400

    Add animation to mobile nav menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,26 +1,69 @@
-import React, { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import StyleColors from "../constants/StyleColors";
 import { useAuth } from "../hooks/AuthProvider";
 
 const Navbar = () => {
 	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-	const toggleMobileMenu = () => {
-		setIsMobileMenuOpen((prev) => !prev);
-	};
+	const mobileMenuRef = useRef();
 	const auth = useAuth();
+	const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setScreenWidth(window.innerWidth);
+		};
+
+		window.addEventListener("resize", handleResize);
+
+		// Cleanup function to remove the event listener when the component unmounts
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+	useEffect(() => {
+		updateMenuHeight();
+	}, [isMobileMenuOpen, screenWidth]);
+
+	const updateMenuHeight = async () => {
+		if (screenWidth < 640) {
+			if (!isMobileMenuOpen) {
+				mobileMenuRef.current.style.maxHeight = "0px";
+			} else {
+				const children = mobileMenuRef.current.children;
+				let totalHeight = 0;
+				for (let i = 0; i < children.length; i++) {
+					const child = children[i];
+					const computedStyle = window.getComputedStyle(child);
+					const marginTop = parseInt(computedStyle.marginTop);
+					const marginBottom = parseInt(computedStyle.marginBottom);
+					totalHeight += child.offsetHeight + marginTop + marginBottom;
+				}
+				mobileMenuRef.current.style.maxHeight = `${totalHeight}px`;
+			}
+		} else {
+			mobileMenuRef.current.style.maxHeight = "";
+		}
+	};
 
 	return (
 		<header className="w-full text-white relative" style={{ backgroundColor: StyleColors.blue }}>
 			<nav className="w-full relative" style={{ borderBottom: "1px solid rgba(235,235,235, 0.5)" }}>
 				<div className="flex flex-col sm:flex-row justify-between items-center py-3 px-5">
 					<div className="block w-full h-10 sm:hidden">
-						<img className="float-left h-full" src="/mobileMenu.svg" onClick={toggleMobileMenu} />
+						<img
+							className="float-left h-full"
+							src="/mobileMenu.svg"
+							onClick={() => setIsMobileMenuOpen((prev) => !prev)}
+						/>
 					</div>
 					<div
-						className={`${
-							!isMobileMenuOpen ? "hidden" : "flex"
-						} items-center w-full sm:flex flex-col sm:flex-row sm:justify-between sm:gap-5`}
+						ref={mobileMenuRef}
+						style={{
+							transition: "all 0.1s linear",
+						}}
+						className={`items-center overflow-hidden w-full flex flex-col sm:flex-row sm:justify-between sm:gap-5`}
 					>
 						<Link className={`link-item mb-2 sm:mb-0 max-sm:hidden flex items-center`} to="/">
 							<img
