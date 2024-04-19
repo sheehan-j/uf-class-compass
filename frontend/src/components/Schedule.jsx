14feb0b80ad817fe689580ca3a7e115ba33c1c2a
Jordan Sheehan <jordansheehan@ufl.edu>
2024-04-19 09:12:25 -0400

    Frontend/backend support for online classes

--- frontend/src/components/Schedule.jsx
+++ frontend/src/components/Schedule.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import Grid from "./Grid";
 import Colors from "../constants/Colors";
@@ -8,6 +8,7 @@ import { getPeriodTimes } from "../constants/BlockTimes";
 import { SchedulesApi } from "../api/SchedulesApi";
 import StyleColors from "../constants/StyleColors";
 import SlidingSidebar from "./SlidingSidebar";
+import OnlineSection from "./OnlineSection";
 
 const Schedule = ({ colCount, maxRowCount, activeSchedule, previewSchedule, handleToggleSidebar }) => {
 	const [grid, setGrid] = useState([]);
@@ -15,6 +16,27 @@ const Schedule = ({ colCount, maxRowCount, activeSchedule, previewSchedule, hand
 	const [newRowCount, setRowCount] = useState(maxRowCount);
 	const [isClassClicked, setIsClassClicked] = useState(false);
 	const [cell, setCell] = useState();
+	const [gridWidth, setGridWidth] = useState(null);
+	const [onlineSections, setOnlineSections] = useState([]);
+	const gridRefMain = useRef(null);
+	const gridRefSkeleton = useRef(null);
+
+	// Step 2: Create a state to hold the width
+	const [width, setWidth] = useState(0);
+
+	// Step 3: Add an event listener for the window resize event
+	useEffect(() => {
+		const handleResize = () => {
+			if (gridRefMain.current) {
+				setWidth(gridRefMain.current.offsetWidth);
+			}
+		};
+		window.addEventListener("resize", handleResize);
+		handleResize();
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
 	useEffect(() => {
 		const rows = [];
@@ -32,40 +54,51 @@ const Schedule = ({ colCount, maxRowCount, activeSchedule, previewSchedule, hand
 		let sections = null;
 		if (previewSchedule?.sections?.length > 0) sections = previewSchedule.sections;
 		else if (activeSchedule?.sections?.length > 0) sections = activeSchedule.sections;
+		const newOnlineSections = [];
 
 		if (sections) {
 			sections.forEach((section) => {
 				totalCredits += section.credits;
-
-				section?.meetings?.forEach((meetingItem) => {
-					rows[meetingItem.period - 1][meetingItem.day] = {
-						// ** Subtract 1 from the period to zero it (all periods stored for sections are 1-based)
-						period: meetingItem.period,
+				if (section?.isOnline) {
+					const onlineSection = {
+						_id: section._id,
 						instructor: section.instructor.name,
-						isClass: true,
 						color: Colors.classColors[colorIndex],
 						code: section.class.code,
-						location: `${meetingItem.building.code} ${meetingItem.room}`,
-						length: meetingItem.length,
-						distance: meetingItem?.distance ? meetingItem.distance : null,
-						displayText: true,
+						location: "Online",
 					};
-
-					for (var i = 1; i < meetingItem.length; i++) {
-						rows[meetingItem.period - 1 + i][meetingItem.day] = {
-							instructor: section.instructor.name,
-							location: `${meetingItem.building.code} ${meetingItem.room}`,
+					newOnlineSections.push(onlineSection);
+				} else {
+					section?.meetings?.forEach((meetingItem) => {
+						rows[meetingItem.period - 1][meetingItem.day] = {
+							// ** Subtract 1 from the period to zero it (all periods stored for sections are 1-based)
 							period: meetingItem.period,
+							instructor: section.instructor.name,
 							isClass: true,
-							code: section.class.code,
 							color: Colors.classColors[colorIndex],
-							displayText: false,
+							code: section.class.code,
+							location: `${meetingItem.building.code} ${meetingItem.room}`,
+							length: meetingItem.length,
+							distance: meetingItem?.distance ? meetingItem.distance : null,
+							displayText: true,
 						};
-					}
-				});
+
+						for (var i = 1; i < meetingItem.length; i++) {
+							rows[meetingItem.period - 1 + i][meetingItem.day] = {
+								instructor: section.instructor.name,
+								location: `${meetingItem.building.code} ${meetingItem.room}`,
+								period: meetingItem.period,
+								isClass: true,
+								code: section.class.code,
+								color: Colors.classColors[colorIndex],
+								displayText: false,
+							};
+						}
+					});
+				}
+
 				colorIndex++;
 			});
-
 			while (rows.length > 1 && !rows[0].some((cell) => cell.isClass) && !rows[1].some((cell) => cell.isClass)) {
 				rows.shift();
 			}
@@ -80,96 +113,114 @@ const Schedule = ({ colCount, maxRowCount, activeSchedule, previewSchedule, hand
 
 			setRowCount(rows.length);
 			setGrid(rows);
-			setCredits(totalCredits);
+			setOnlineSections(newOnlineSections);
 		}
 	}, [colCount, maxRowCount, activeSchedule, previewSchedule]);
 
 	return (
-		<div className="pl-3 pr-1 sm:px-10 py-20 w-full min-h-full flex relative overflow-x-clip">
-			<div className="absolute top-5 right-5 font-bold p-2">CREDITS: {credits}</div>
-			<div
-				className="flex pl-10 pr-3 rounded-r-md align-items-center absolute top-5 text-white left-0 font-bold lg:hidden p-2 hover:cursor-pointer bg-customOrange hover:bg-customOrange-dark"
-				onClick={handleToggleSidebar}
-			>
-				{/* View Schedules */}
-				<img src="./expand_right.svg" className="h-7 w-auto" />
-			</div>
-			<div className="flex flex-col mr-2 sm:mr-3 w-4 sm:w-1/12">
+		<div className="pl-3 pr-1 sm:px-10 py-20 w-full min-h-full flex flex-col relative overflow-x-clip">
+			<div className="flex flex-row grow">
+				<div className="absolute top-5 right-5 font-bold p-2">CREDITS: {credits}</div>
 				<div
-					className="font-medium"
-					style={{
-						minHeight: "2.5rem",
-						whiteSpace: "nowrap",
-						marginBottom: "2rem",
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-					}}
-				></div>
-				<div className="h-full flex flex-col">
-					{grid.map((row, index) => (
-						<div key={index} className="relative flex grow items-center justify-end">
-							<div
-								className="italic absolute right-4 lg:right-6"
-								style={{ whiteSpace: "nowrap", fontSize: "0.9rem", top: "-0.45rem" }}
-							>
-								<span className="hidden sm:block">{getPeriodTimes(row[0].period).start}</span>
-							</div>
-							<div
-								style={{
-									flex: "1",
-									display: "flex",
-									justifyContent: "right",
-									alignItems: "center",
-								}}
-							>
-								{row[0].period}
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
-
-			<div className="flex flex-col w-full grow">
-				<div
-					className="grid w-full relative"
-					style={{
-						marginBottom: "2rem",
-						height: "2.5rem",
-						gridTemplateColumns: `repeat(${colCount}, minmax(0, 1fr))`,
-					}}
+					className="flex pl-10 pr-3 rounded-r-md align-items-center absolute top-5 text-white left-0 font-bold lg:hidden p-2 hover:cursor-pointer bg-customOrange hover:bg-customOrange-dark"
+					onClick={handleToggleSidebar}
 				>
-					<DaysBox />
+					{/* View Schedules */}
+					<img src="./expand_right.svg" className="h-7 w-auto" />
+				</div>
+				<div className="flex flex-col mr-2 sm:mr-3 w-4 sm:w-1/12">
+					<div
+						className="font-medium"
+						style={{
+							minHeight: "2.5rem",
+							whiteSpace: "nowrap",
+							marginBottom: "2rem",
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+						}}
+					></div>
+					<div className="h-full flex flex-col">
+						{grid.map((row, index) => (
+							<div key={index} className="relative flex grow items-center justify-end">
+								<div
+									className="italic absolute right-4 lg:right-6"
+									style={{ whiteSpace: "nowrap", fontSize: "0.9rem", top: "-0.45rem" }}
+								>
+									<span className="hidden sm:block">{getPeriodTimes(row[0].period).start}</span>
+								</div>
+								<div
+									style={{
+										flex: "1",
+										display: "flex",
+										justifyContent: "right",
+										alignItems: "center",
+									}}
+								>
+									{row[0].period}
+								</div>
+							</div>
+						))}
+					</div>
 				</div>
 
-				<div className="w-full grow border border-slate-300 relative">
-					{/* 
+				<div className="flex flex-col w-full grow">
+					<div
+						className="grid w-full relative"
+						style={{
+							marginBottom: "2rem",
+							height: "2.5rem",
+							gridTemplateColumns: `repeat(${colCount}, minmax(0, 1fr))`,
+						}}
+					>
+						<DaysBox />
+					</div>
+
+					<div className="w-full grow relative">
+						{/* 
 				There are TWO grids, one is a skeleton that creates the grid lines,
 				the other is the grid actually containing the sections and their colored
 				rectangles. This is done to achieve the visual effect of the grid lines
 				behind the transparent class boxes.
 				*/}
-					<Grid
-						key={"skeletonGrid"}
-						grid={grid}
-						colCount={colCount}
-						rowCount={newRowCount}
-						isSkeleton={false}
-						isClassClicked={isClassClicked}
-						setIsClassClicked={setIsClassClicked}
-						setCell={setCell}
-					/>
-					<Grid
-						key={"coloredGrid"}
-						grid={grid}
-						colCount={colCount}
-						rowCount={newRowCount}
-						isSkeleton={true}
-						isClassClicked={isClassClicked}
-						setIsClassClicked={setIsClassClicked}
-						setCell={setCell}
-					/>
+						<Grid
+							key={"skeletonGrid"}
+							grid={grid}
+							colCount={colCount}
+							rowCount={newRowCount}
+							isSkeleton={false}
+							isClassClicked={isClassClicked}
+							setIsClassClicked={setIsClassClicked}
+							setCell={setCell}
+							gridRef={gridRefSkeleton}
+						/>
+						<Grid
+							key={"coloredGrid"}
+							grid={grid}
+							colCount={colCount}
+							rowCount={newRowCount}
+							isSkeleton={true}
+							isClassClicked={isClassClicked}
+							setIsClassClicked={setIsClassClicked}
+							setCell={setCell}
+							gridRef={gridRefMain}
+						/>
+					</div>
 				</div>
+			</div>
+			<div className="w-full flex flex-col items-end">
+				{/* <OnlineSection width={width} /> */}
+				{onlineSections.map((onlineSection) => (
+					<OnlineSection
+						key={onlineSection._id}
+						width={width}
+						cell={onlineSection}
+						onCellClick={() => {
+							setIsClassClicked(true);
+							setCell(onlineSection);
+						}}
+					/>
+				))}
 			</div>
 			<SlidingSidebar isClassClicked={isClassClicked} setIsClassClicked={setIsClassClicked} cell={cell} />
 		</div>
