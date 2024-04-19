6ef23ffcebd8f396fb98463ccb45eab6933a42ee
Jordan Sheehan <jordansheehan@ufl.edu>
2024-04-18 23:54:01 -0400

    Delete more duplicate files

--- backend/routes/distance 2.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const DistanceController = require("../controllers/DistanceController");
-
-router.route("/").get(DistanceController.getDistance);
-
-module.exports = router;
