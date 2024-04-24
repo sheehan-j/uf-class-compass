1864d9efcadb5a5db0c6d9c2e61f63045985256a
Jordan Sheehan <jordansheehan@ufl.edu>
2024-04-24 11:54:16 -0400

    Remove distance when hovering class and set height limit for course sections

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -29,25 +29,25 @@ const Sidebar = ({
 	const [showAutoComplete, setShowAutoComplete] = useState(true);
 	const autoCompleteRef = useRef(null);
 
-	const handleChangeSearchTerm = async(cls) => {
+	const handleChangeSearchTerm = async (cls) => {
 		const code = cls.toUpperCase();
-		setSearchTerm(code)
-		if(code.length == 0){
+		setSearchTerm(code);
+		if (code.length == 0) {
 			setClassByPrefix([]);
 			return;
 		}
 		let result = await ClassesApi.getClassSectionPrefix(code);
-		if(result.length === 0) {
+		if (result.length === 0) {
 			setClassByPrefix([]);
-		}else{
+		} else {
 			setClassByPrefix(result.map((cls) => cls.code));
 		}
-	}
-	
+	};
+
 	const handleClickAutocomplete = (code) => {
 		setSearchTerm(code);
 		setClassByPrefix([]);
-	}
+	};
 
 	const handleCourseCodeButtonClicked = async (section) => {
 		let result = await ClassesApi.getClassSections(section.class.code);
@@ -131,6 +131,12 @@ const Sidebar = ({
 			updatedActiveSchedule.sections = updatedActiveSchedule.sections.filter(
 				(activeScheduleClass) => activeScheduleClass.class.code != section.class.code
 			);
+			updatedActiveSchedule.sections = updatedActiveSchedule.sections.map((section) => {
+				const meetingsWithoutDistance = section.meetings.map((meeting) => {
+					return { ...meeting, distance: null };
+				});
+				return { ...section, meetings: meetingsWithoutDistance };
+			});
 			updatedActiveSchedule.sections.push({ ...section, muteInActiveCourses: true });
 			setPreviewSchedule(updatedActiveSchedule);
 		}
@@ -153,19 +159,18 @@ const Sidebar = ({
 	}, [activeSchedule]);
 
 	useEffect(() => {
-        const handleOutsideClick = (event) => {
+		const handleOutsideClick = (event) => {
 			if (autoCompleteRef.current && !autoCompleteRef.current.contains(event.target)) {
-                setShowAutoComplete(false);
-            } else {
+				setShowAutoComplete(false);
+			} else {
 				setShowAutoComplete(true);
 			}
-        };
-        document.body.addEventListener('mousedown', handleOutsideClick);
-        return () => {
-            document.body.removeEventListener('mousedown', handleOutsideClick);
-        };
-    }, []);
-
+		};
+		document.body.addEventListener("mousedown", handleOutsideClick);
+		return () => {
+			document.body.removeEventListener("mousedown", handleOutsideClick);
+		};
+	}, []);
 
 	return (
 		<div
@@ -177,100 +182,109 @@ const Sidebar = ({
 			className="min-h-full top-0 w-full md:w-7/12 text-black absolute lg:w-1/4 lg:sticky overflow-x-visible z-50 border-r border-gray-300 lg:border-none"
 		>
 			<div className="sticky top-0 overflow-y-visible h-fit p-5">
-			<div className="mb-3 lg:hidden w-full flex justify-end">
-				<button onClick={handleToggleSidebar}>
-					<img src="/remove.svg" />
-				</button>
-			</div>
+				<div className="mb-3 lg:hidden w-full flex justify-end">
+					<button onClick={handleToggleSidebar}>
+						<img src="/remove.svg" />
+					</button>
+				</div>
 
-			<MySchedules
-				schedules={schedules}
-				setSchedules={setSchedules}
-				activeSchedule={activeSchedule}
-				setActiveSchedule={setActiveSchedule}
-				activeClass={activeClass}
-				setActiveClass={setActiveClass}
-				classResults={classResults}
-				setClassResults={setClassResults}
-			/>
-
-			{activeSchedule?.sections?.length > 0 && (
-				<>
-					<p className="mb-1">Active Courses</p>
-					<div className="mb-4">
-						{activeSchedule?.sections
-							?.filter((section) => section?.muteInActiveCourses != true)
-							?.map((section) => (
-								<CourseCodeButton
-									key={section?.number}
-									section={section}
-									active={activeClass == section?.class?.code}
-									handleCourseCodeButtonClicked={handleCourseCodeButtonClicked}
-									handleDeleteClass={handleDeleteClass}
-								/>
-							))}
-					</div>
-				</>
-			)}
-
-			<p className={`mb-1 ${activeSchedule?.sections ? "" : "opacity-40"}`}>Course Code Search</p>
-			<div className="relative w-full" ref={autoCompleteRef}>
-				<input
-					className={`w-full py-2 px-2 flex align-center bg-white border border-gray-300 ${
-						activeSchedule?.sections ? "" : "opacity-60 line-through"
-					}`}
-					placeholder="Enter class code (e.g. CIS4930)"
-					value={searchTerm}
-					onChange={(e) => handleChangeSearchTerm(e.target.value)}
-					onKeyUp={handleSearchInputKeyUp}
-					disabled={!activeSchedule?.sections}
+				<MySchedules
+					schedules={schedules}
+					setSchedules={setSchedules}
+					activeSchedule={activeSchedule}
+					setActiveSchedule={setActiveSchedule}
+					activeClass={activeClass}
+					setActiveClass={setActiveClass}
+					classResults={classResults}
+					setClassResults={setClassResults}
 				/>
-				{classByPrefix && (
-					<div className={`absolute z-50 w-full ${showAutoComplete ? "block" : "hidden"} w-full`} ref={autoCompleteRef}>
-						{classByPrefix.map((classCode, index) => {
-							const prefixIdx = classCode.toUpperCase().indexOf(searchTerm.toUpperCase());
-							const toSearch = classCode;
-							const nonBolded = classCode.substring(prefixIdx + searchTerm.length);
-							
-							return (
-								<div className="w-full py-2 px-2 flex align-center bg-gray-100 border border-gray-300 cursor-pointer" key={index} onClick={() => handleClickAutocomplete(toSearch)}>
-									<strong>{searchTerm.toUpperCase()}</strong>
-									<span>{nonBolded}</span>
-								</div>
-							);
-						})}
-					</div>
-				)}
-			</div>
-			<button
-				className={`w-full text-white bg-customBlue hover:bg-customBlue-dark py-2 rounded-lg mt-4 ${
-					searchError == "" && activeSchedule?.sections ? "mb-4" : "mb-1"
-				} ${activeSchedule?.sections ? "" : "opacity-60"}`}
-				onClick={handleSearch}
-			>
-				Search
-			</button>
-			{searchError != "" && <p className="text-red-400 text-sm mb-4">{searchError}</p>}
-			{!activeSchedule?.sections && (
-				<p className="italic text-sm mb-4">Select or create a schedule to start searching for classes.</p>
-			)}
 
-			{activeClass && (
-				<>
-					<p className="mb-1">Course Sections &#40;{activeClass}&#41;</p>
-					{classResults?.length > 0 &&
-						classResults?.map((section) => (
-							<CourseSectionBox
-								key={section.number}
-								section={section}
-								handleAddSection={handleAddSection}
-								activeSchedule={activeSchedule}
-								handleHoverSectionStart={handleHoverSectionStart}
-								handleHoverSectionEnd={handleHoverSectionEnd}
-							/>
-						))}
-				</>
-			)}
+				{activeSchedule?.sections?.length > 0 && (
+					<>
+						<p className="mb-1">Active Courses</p>
+						<div className="mb-4">
+							{activeSchedule?.sections
+								?.filter((section) => section?.muteInActiveCourses != true)
+								?.map((section) => (
+									<CourseCodeButton
+										key={section?.number}
+										section={section}
+										active={activeClass == section?.class?.code}
+										handleCourseCodeButtonClicked={handleCourseCodeButtonClicked}
+										handleDeleteClass={handleDeleteClass}
+									/>
+								))}
+						</div>
+					</>
+				)}
+
+				<p className={`mb-1 ${activeSchedule?.sections ? "" : "opacity-40"}`}>Course Code Search</p>
+				<div className="relative w-full" ref={autoCompleteRef}>
+					<input
+						className={`w-full py-2 px-2 flex align-center bg-white border border-gray-300 ${
+							activeSchedule?.sections ? "" : "opacity-60 line-through"
+						}`}
+						placeholder="Enter class code (e.g. CIS4930)"
+						value={searchTerm}
+						onChange={(e) => handleChangeSearchTerm(e.target.value)}
+						onKeyUp={handleSearchInputKeyUp}
+						disabled={!activeSchedule?.sections}
+					/>
+					{classByPrefix && (
+						<div
+							className={`absolute z-50 w-full ${showAutoComplete ? "block" : "hidden"} w-full`}
+							ref={autoCompleteRef}
+						>
+							{classByPrefix.map((classCode, index) => {
+								const prefixIdx = classCode.toUpperCase().indexOf(searchTerm.toUpperCase());
+								const toSearch = classCode;
+								const nonBolded = classCode.substring(prefixIdx + searchTerm.length);
+
+								return (
+									<div
+										className="w-full py-2 px-2 flex align-center bg-gray-100 border border-gray-300 cursor-pointer"
+										key={index}
+										onClick={() => handleClickAutocomplete(toSearch)}
+									>
+										<strong>{searchTerm.toUpperCase()}</strong>
+										<span>{nonBolded}</span>
+									</div>
+								);
+							})}
+						</div>
+					)}
+				</div>
+				<button
+					className={`w-full text-white bg-customBlue hover:bg-customBlue-dark py-2 rounded-lg mt-4 ${
+						searchError == "" && activeSchedule?.sections ? "mb-4" : "mb-1"
+					} ${activeSchedule?.sections ? "" : "opacity-60"}`}
+					onClick={handleSearch}
+				>
+					Search
+				</button>
+				{searchError != "" && <p className="text-red-400 text-sm mb-4">{searchError}</p>}
+				{!activeSchedule?.sections && (
+					<p className="italic text-sm mb-4">Select or create a schedule to start searching for classes.</p>
+				)}
+
+				{activeClass && (
+					<>
+						<p className="mb-1">Course Sections &#40;{activeClass}&#41;</p>
+						<div className="overflow-y-auto" style={{ maxHeight: "36rem" }}>
+							{classResults?.length > 0 &&
+								classResults?.map((section) => (
+									<CourseSectionBox
+										key={section.number}
+										section={section}
+										handleAddSection={handleAddSection}
+										activeSchedule={activeSchedule}
+										handleHoverSectionStart={handleHoverSectionStart}
+										handleHoverSectionEnd={handleHoverSectionEnd}
+									/>
+								))}
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
