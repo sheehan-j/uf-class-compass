900b6888e44e885e38ded9c7df10357e25c7619a
Jordan Sheehan <jordansheehan@ufl.edu>
2024-04-24 11:56:03 -0400

    Function to decrement all days in the DB

--- backend/controllers/ClassController.js
+++ backend/controllers/ClassController.js
@@ -15,7 +15,7 @@ exports.getClass = async (req, res) => {
 
 exports.getClassPrefix = async (req, res) => {
 	try {
-		const result = await Class.find({ code: { $regex: `^${req.query.code}`, $options: 'i' } });
+		const result = await Class.find({ code: { $regex: `^${req.query.code}`, $options: "i" } });
 		if (!result) return res.status(204).json({ message: "No class with that prefix found not found." });
 		return res.status(200).json(result);
 	} catch (err) {
@@ -49,5 +49,51 @@ exports.getSections = async (req, res) => {
 	} catch (err) {
 		console.error("Error:", err);
 		return res.status(500).json({ message: "Internal server error." });
+	}
+};
+
+// *** This is not a function that should be used regularly, it was used
+// to decrement all days in the DB since they were originally 1-based (Monday = 1)
+// and needed to be 0-based, keeping this here in just case
+const decrementAllDays = async (req, res) => {
+	try {
+		let sections = await Section.find({});
+
+		sections = sections.map((section) => {
+			if (section.meetings.length == 0) return section;
+			const newMeetings = section.meetings.map((meeting) => {
+				return {
+					period: meeting.period,
+					length: meeting.length,
+					building: meeting.building,
+					room: meeting.room,
+					day: meeting.day - 1,
+					_id: meeting._id,
+				};
+			});
+			return {
+				_id: section._id,
+				number: section.number,
+				class: section.class,
+				instructor: section.instructor,
+				credits: section.credits,
+				final: section.final,
+				department: section.department,
+				isOnline: section.isOnline,
+				meetings: newMeetings,
+			};
+		});
+
+		let updateIndex = 0;
+		for (const section of sections) {
+			console.log(`Updating section ${updateIndex} starting...`);
+			await Section.updateOne({ _id: section._id }, { $set: { meetings: section.meetings } });
+			console.log(`Updating section ${updateIndex} complete.`);
+			updateIndex++;
+		}
+
+		return res.status(200).json("yer");
+	} catch (err) {
+		return res.status(500).json("error");
 	}
 };
