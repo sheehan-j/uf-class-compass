170258acace2878a61b48cb6573db7dbe228893d
Jordan Sheehan <jordansheehan@ufl.edu>
2024-04-25 15:19:02 -0400

    Frontend course search implementation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,10 +10,12 @@ export default {
 				customOrange: {
 					DEFAULT: "rgb(235, 120, 23)",
 					dark: "rgb(209, 104, 15)",
+					transparent: "rgba(235,120,23,0.2)",
 				},
 				customBlue: {
 					DEFAULT: "rgb(40, 87, 151)",
 					dark: "rgb(27, 69, 128)",
+					transparent: "rgba(40, 87, 151, 0.2)",
 				},
 				customGray: {
 					DEFAULT: "rgb(235, 235, 235)",
