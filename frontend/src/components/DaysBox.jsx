fc93da15b1252f18beae59471d6c719669341f49
Jordan Sheehan <jordansheehan@ufl.edu>
2024-04-29 15:29:56 -0400

    Remove schedule accent for current day

--- frontend/src/components/DaysBox.jsx
+++ frontend/src/components/DaysBox.jsx
@@ -2,20 +2,20 @@ import { Days, getDayString } from "../constants/Days";
 import { useState, useEffect } from "react";
 const DaysBox = () => {
 	const currentDate = new Date().getDay() - 1; // Adjusting for Monday start
-	const [isMobileScreen, setIsMobileScreen] = useState(window.matchMedia("(max-width: 640px)").matches)
+	const [isMobileScreen, setIsMobileScreen] = useState(window.matchMedia("(max-width: 640px)").matches);
 
 	useEffect(() => {
 		const handleResize = () => {
-		  setIsMobileScreen(window.matchMedia("(max-width: 640px)").matches);
+			setIsMobileScreen(window.matchMedia("(max-width: 640px)").matches);
 		};
-	
+
 		window.addEventListener("resize", handleResize);
-	
+
 		return () => {
-		  window.removeEventListener("resize", handleResize);
+			window.removeEventListener("resize", handleResize);
 		};
-	  }, []);
-	
+	}, []);
+
 	// Convert enum to an array of day names
 	const daysArray = Object.keys(Days).map((key) => Days[key]);
 
@@ -24,8 +24,9 @@ const DaysBox = () => {
 			{daysArray.map((day, index) => (
 				<div
 					key={"dayWrapper" + index}
-					className={`flex justify-center items-center h-full w-full ${isMobileScreen ? "rounded-l" : "rounded-3xl"} 
-						${index === currentDate? "bg-customBlue text-white border-none" : "bg-customGray text-[#8F8F8F] border border-solid border-[#DEDEDE]"}`}
+					className={`flex justify-center items-center h-full w-full bg-customGray text-[#8F8F8F] border border-solid border-[#DEDEDE] ${
+						isMobileScreen ? "rounded-l" : "rounded-3xl"
+					}`}
 				>
 					{!isMobileScreen ? getDayString(day) : getDayString(day).charAt(0)}
 				</div>
